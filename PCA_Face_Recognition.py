daef8e4a9e300a65dc4a1db29cdf30b1c95f2a07
Jusang_ <0504won@gmail.com>
2021-06-10 22:02:09 +0900

    Update PCA_Face_Recognition.py

--- PCA_Face_Recognition.py
+++ PCA_Face_Recognition.py
@@ -5,3 +5,10 @@ from PIL import Image
 #파일 불러오기
 path_dir = '/Users/junsangwon/Downloads/archive'
 file_list = os.listdir(path_dir)
+
+#이미지파일 배열(n,1)로 변환한기
+def mk_ar(a):
+    np_array = np.array(a)
+    len_np_array = np_array.shape[0]*np_array.shape[1]
+    np_array_res = np.array(a).reshape(len_np_array, 1)
+    return np_array_res
